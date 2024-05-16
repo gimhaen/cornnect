d2bef217616e8ca1191cc24560206dfa48e36089
kke0402 <kke0402@gmail.com>
2024-05-16 14:07:39 +0900

    :tada: create cornnect

---
+++ vue/cornnect/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <div>
+      <nav>
+        <RouterLink to="/">홈</RouterLink>
+        <RouterLink to="/">검색</RouterLink>
+        <RouterLink to="/">영화</RouterLink>
+        <RouterLink to="/">프로필</RouterLink>
+      </nav>
+    </div>
+  </header>
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterView, RouterLink } from 'vue-router'
+</script>
+
+<style scoped>
+
+</style>
